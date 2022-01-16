801b6128f12742ed318656d27ca66d2d241bf915
Gökberk Akalın <gokberkakalin@gmail.com>
2022-01-16 15:44:24 +0300

    parallel_breakingCancellationExceptions

---
+++ parallel_breakingCancellationExceptions/parallel_breakingCancellationExceptions/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace parallel_breakingCancellationExceptions
+{
+    internal class Program
+    {
+        private static ParallelLoopResult result;
+        public static void Demo()
+        {
+            var cts = new CancellationTokenSource();
+
+            ParallelOptions po = new ParallelOptions();
+            po.CancellationToken = cts.Token;
+            
+            result = Parallel.For(0, 20, po, (int x, ParallelLoopState state) =>
+            {
+                Console.WriteLine($"{x}[{Task.CurrentId}]\t");
+                if (x == 10)
+                {
+                    // throw new Exception();
+                    //state.Stop();
+                    //state.Break();
+                    cts.Cancel();
+                }
+            });
+            Console.WriteLine();
+            Console.WriteLine($"was loop completed? {result.IsCompleted}");
+            if (result.LowestBreakIteration.HasValue)
+            {
+                Console.WriteLine($"lowest break iteration is {result.LowestBreakIteration}");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Demo();
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    Console.WriteLine(e.Message);
+                    return true;
+                });
+            }
+            catch (OperationCanceledException oce)
+            {
+                Console.WriteLine(oce.Message);
+            }
+        }
+    }
+}
