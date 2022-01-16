2de29307da959d5adbec2c9a5c2415ee60de4b45
Gökberk Akalın <gokberkakalin@gmail.com>
2022-01-16 19:55:59 +0300

    parallel_async_await

---
+++ parallel_async_await/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parallel_async_await
+{
+    public partial class Form1 : Form
+    {
+
+        public int CalculateValue()
+        {
+            Thread.Sleep(5000);
+            return 123;
+        }
+
+        public async Task<int> CalculateValueAsync()
+        {
+            await Task.Delay(5000);
+            return 123;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            int value = await CalculateValueAsync();
+            LblResult.Text = value.ToString();
+
+            await Task.Delay(5000);
+
+            using (var wc = new WebClient())
+            {
+                string data = await
+                    wc.DownloadStringTaskAsync("http://google.com/robots.txt");
+                LblResult.Text = data.Split('\n')[0].Trim();
+            }
+
+        }
+    }
+}
