f98ab26f6f44052f5252c3087f11ddce6575b44a
Gökberk Akalın <gokberkakalin@gmail.com>
2022-01-16 15:27:21 +0300

    parallel_loops

---
+++ parallel_loops/parallel_loops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace parallel_loops
+{
+    internal class Program
+    {
+        public static IEnumerable<int> Range(int start, int end, int step)
+        {
+            for (int i = start; i < end; i += step)
+            {
+                yield return i;
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            var a = new Action(() => Console.WriteLine($"first {Task.CurrentId}"));
+            var b = new Action(() => Console.WriteLine($"second {Task.CurrentId}"));
+            var c = new Action(() => Console.WriteLine($"third {Task.CurrentId}"));
+            
+            Parallel.Invoke(a,b,c);
+            
+            Parallel.For(1, 11, i =>
+            {
+                Console.WriteLine($"{i * i}\t");
+            });
+
+            string[] words = {"oh", "what", "a", "day"};
+
+            Parallel.ForEach(words, word =>
+            {
+                Console.WriteLine($"{word} has length {word.Length} (task {Task.CurrentId})");
+            });
+
+            Parallel.ForEach(Range(1, 20, 3), Console.WriteLine);
+        }
+    }
+}
