f1d3497996c62248b319fc0ff386c34ae2bb7827
Gökberk Akalın <gokberkakalin@gmail.com>
2022-01-16 19:00:37 +0300

    plinq_custom_agregation

---
+++ plinq_custom_aggregation/plinq_custom_aggregation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace plinq_custom_aggregation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var sum = Enumerable.Range(1, 1000).Sum();
+
+            //var sum = Enumerable.Range(1, 1000)
+            //    .Aggregate(0, (i, acc) => i+acc);
+
+            var sum = ParallelEnumerable.Range(1, 1000)
+                .Aggregate(
+                    0,
+                    (partialSum, i) => partialSum += i,
+                    (total, subTotal) => total += subTotal,
+                    i => i);
+
+            Console.WriteLine($"sum = {sum}");
+            
+        }
+    }
+}
