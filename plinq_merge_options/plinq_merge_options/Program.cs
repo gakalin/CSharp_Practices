7ff90a2cbb1a93bc811f0e62e834357d40f8f143
Gökberk Akalın <gokberkakalin@gmail.com>
2022-01-16 18:55:08 +0300

    plinq_merge_options

---
+++ plinq_merge_options/plinq_merge_options/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace plinq_merge_options
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 20).ToArray();
+
+            var results = numbers
+                .AsParallel()
+                .WithMergeOptions(ParallelMergeOptions.FullyBuffered)
+                .Select(x =>
+                {
+                    var result = Math.Log10(x);
+                    Console.Write($"p {result}\t");
+                    return result;
+                });
+
+            foreach (var result in results)
+            {
+                Console.Write($"c {result}\t");
+            }
+        }
+    }
+}
