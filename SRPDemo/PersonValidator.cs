e168720e0a34ef2f7cab6aab2e3602b569a53fd2
Gökberk Akalın <gokberkakalin@gmail.com>
2022-04-03 19:27:04 +0300

    SRP

---
+++ SRPDemo/PersonValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPDemo
+{
+    public class PersonValidator
+    {
+        public static bool Validate(Person person)
+        {
+            if (string.IsNullOrWhiteSpace(person.FirstName))
+            {
+                StandartMessages.DisplayValidationError("first name");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(person.LastName))
+            {
+                StandartMessages.DisplayValidationError("last name");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SRPDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using SRPDemo;
+
+StandartMessages.WelcomeMessage();
+
+Person user = PersonDataCapture.Capture();
+
+bool isUserValid = PersonValidator.Validate(user);
+
+if (!isUserValid)
+{
+    StandartMessages.EndApplication();
+    return;
+}
+
+AccountGenerator.CreateAccount(user);
+StandartMessages.EndApplication();
