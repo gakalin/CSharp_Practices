97e28bc36ce0bbfd1f20566b42c157edb1f581c5
Gökberk Akalın <gokberkakalin@gmail.com>
2022-04-09 21:15:06 +0300

    LSP

---
+++ DemoLibrary/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoLibrary
+{
+    public class Employee : BaseEmployee, IManaged
+    {
+        public IEmployee Manager { get; set; } = null;
+
+        public virtual void AssignManager(IEmployee manager)
+        {
+            Manager = manager;
+        }
+    }
+}
---
+++ LSP/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoLibrary;
+
+IManager accountingVP = new Manager();
+accountingVP.FirstName = "Gokberk";
+accountingVP.LastName = "Akalin";
+accountingVP.CalculatePerHourRate(4);
+
+IManaged emp = new Manager();
+
+emp.FirstName = "Erdem";
+emp.LastName = "Akalin";
+emp.AssignManager(accountingVP);
+emp.CalculatePerHourRate(2);
+
+Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour");
+
+Console.ReadLine();
