b1bf425323f089f219c03d79cfb95afa3cd31f83
Gökberk Akalın <gokberkakalin@gmail.com>
2022-04-09 16:05:19 +0300

    OCP

---
+++ OCPDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using OCPLibrary;
+
+List<IApplicantModel> applicants = new List<IApplicantModel>
+{
+    new PersonModel { FirstName = "Gokberk", LastName = "Akalin"},
+    new ManagerModel { FirstName = "Test", LastName = "User" },
+    new ExecutiveModel { FirstName = "Erdem", LastName = "Akalın"}
+};
+
+List<EmployeeModel> employees = new List<EmployeeModel>();
+
+foreach (var person in applicants)
+{
+    employees.Add(person.AccountProcessor.Create(person));
+}
+
+foreach (var emp in employees)
+{
+    Console.WriteLine($"{ emp.FirstName } { emp.LastName }: { emp.EmailAddress } IsManager: { emp.IsManager } IsExecutive: { emp.IsExecutive}");
+}
+
+Console.ReadLine();
